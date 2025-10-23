1d41b219f57a4894367f0d256f9abca026d45fd5
Foster <fosterjameskiatarae@gmail.com>
2025-10-23 21:10:20 +1300

    feat(asset_manager.py): Implement core and level asset loading methods

--- scripts/utilities/asset_manager.py
+++ scripts/utilities/asset_manager.py
@@ -1,3 +1,6 @@
+import pygame
+import os
+
 class AssetManager:
     # Core assets to always be loaded
     core_sprites = {}
@@ -9,18 +12,34 @@ class AssetManager:
     level_sounds = {}
     level_fonts = {}
     
+    # Pointers to directories
+    data_dir = os.path.join("data")
+    assets_dir = os.path.join(data_dir, "assets")
+    fonts_dir = os.path.join(assets_dir, 'fonts')
+    sprites_dir = os.path.join(assets_dir, "sprites")
+    environment_dir = os.path.join(sprites_dir, 'environment')
+    biome_dir = os.path.join(environment_dir, 'biome')
+    stringstar_fields_dir = os.path.join(environment_dir, 'stringstar_fields')
+    
     @staticmethod
     def load_core_assets() -> None:
         """load UI, player and common assets."""
-        pass
-    
+        AssetManager.core_fonts['default'] = pygame.font.Font(os.path.join(AssetManager.fonts_dir,'Virtupetpixies-7O3GV.ttf'), 32)
+        AssetManager.core_fonts['Title'] = pygame.font.Font(os.path.join(AssetManager.fonts_dir,'Virtupetpixies-7O3GV.ttf'), 65)
+        AssetManager.core_fonts['small'] = pygame.font.Font(os.path.join(AssetManager.fonts_dir,'Virtupetpixies-7O3GV.ttf'), 20)
+
     @staticmethod
     def load_level_assets(level_name: str) -> None:
         """Load level specific assets.
         Args:
             level_name (str): The name of the level to load the assets for.
         """
-        pass
+        match(level_name):
+            case 'starstring_fields':
+                AssetManager.level_sprites['bg0'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_dir, 'background', '0.png'))
+                AssetManager.level_sprites['bg1'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_dir, 'background', '1.png'))
+                AssetManager.level_sprites['bg2'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_dir, 'background', '2.png'))
+                
     
     @staticmethod
     def unload_level_assets() -> None:
