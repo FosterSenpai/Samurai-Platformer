2b4755798c1b9faf16446734c333f4edba59e74e
Foster <fosterjameskiatarae@gmail.com>
2025-10-23 22:43:17 +1300

    feat(title_state.py): Add background sprite rendering

--- scripts/screen_states/title_state.py
+++ scripts/screen_states/title_state.py
@@ -5,14 +5,20 @@
 # Last Modified: 23-10-2025
 from scripts.screen_states.screen_state import State
 from scripts.utilities.text import Text
+from scripts.utilities.asset_manager import AssetManager
 
 class TitleState(State):
     def __init__(self, game) -> None:
         super().__init__(game)
-        
+        self.bg0 = AssetManager.core_sprites['bg0']
+        self.bg1 = AssetManager.core_sprites['bg1']
+        self.bg2 = AssetManager.core_sprites['bg2']
+
     def update(self, delta_time: float, actions: dict) -> None:
         self.game.controller.reset_actions()
     
     def render(self, surface) -> None:
-        surface.fill((255,255,255))
+        surface.blit(self.bg0, (0,0))
+        surface.blit(self.bg1, (0,0))
+        surface.blit(self.bg2, (0,0))
         Text.draw_text(surface, "Title Screen", 'black', 100,30)
