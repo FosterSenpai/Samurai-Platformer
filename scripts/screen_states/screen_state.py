0aea20a58ec2b6b25d00d49138807516f35271be
Foster <fosterjameskiatarae@gmail.com>
2025-10-23 21:11:05 +1300

    feat(screen_state.py): Add base class for managing screen states in the game

---
+++ scripts/screen_states/screen_state.py
@@ -0,0 +1,27 @@
+# screen_state.py
+# Base class for different screen states in the game.
+# Author: Foster Rae
+# Created: 23-10-2025
+# Last Modified: 23-10-2025
+from game import Game
+import pygame
+
+class State():
+    def __init__(self, game: Game) -> None:
+        self.game = game
+        self.prev_state = None
+        
+    def update(self, delta_time: float, actions: dict) -> None: 
+        pass
+    
+    def render(self, surface: pygame.Surface) -> None:
+        pass
+    
+    def enter_state(self):
+        # If state exists in stack, store as prev state
+        if len(self.game.screen_state_stack) > 1:
+            self.prev_state = self.game.screen_state_stack[-1]
+        self.game.screen_state_stack.append(self)
+        
+    def exit_state(self):
+        self.game.screen_state_stack.pop()
