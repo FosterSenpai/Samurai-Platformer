2a37b0b9d44b9f60929e2e7e6d57e58f5364ae09
Foster <fosterjameskiatarae@gmail.com>
2025-10-24 02:24:03 +1300

    feat(title_state.py): refactor some rendering into methods, add bottons for menu screen with relative spacing and sizing.

--- scripts/screen_states/title_state.py
+++ scripts/screen_states/title_state.py
@@ -11,8 +11,17 @@ from scripts.utilities.asset_manager import AssetManager
 class TitleState(State):
     def __init__(self, game) -> None:
         super().__init__(game)
+        canvas_w, canvas_h = self.game.GAME_W, self.game.GAME_H
         # UI elements
-        self.ui_1 = AssetManager.core_sprites['ui_sprite159'].copy()
+        big_box_width, big_box_height = int(canvas_w * 0.35), int(canvas_h * 0.8)
+        self.ui_big_box = pygame.transform.scale(AssetManager.core_sprites['ui_sprite159'], (big_box_width, big_box_height))
+        menu_button_width, menu_button_height = int(big_box_width * 0.7), int(big_box_height * 0.15)
+        self.ui_button = pygame.transform.scale(AssetManager.core_sprites['ui_sprite205'], (menu_button_width, menu_button_height))
+        self.ui_button_active = AssetManager.core_sprites['ui_sprite206'].copy()
+        self.ui_play_button = self.ui_button.copy()
+        self.ui_load_button = self.ui_button.copy()
+        self.ui_options_button = self.ui_button.copy()
+        self.ui_quit_button = self.ui_button.copy()
         # Background
         self.bg0 = AssetManager.core_sprites['bg0']
         self.bg1 = AssetManager.core_sprites['bg1']
@@ -28,31 +37,18 @@ class TitleState(State):
         self.scroll_bg(delta_time)
         self.game.controller.reset_actions()
     
-    def render(self, surface) -> None:
-        # Draw background
-        # Drawing each image a second time offset to remove scoll seams
-        surface.blit(self.bg0, (self.bg0_x, 0))
-        surface.blit(self.bg0, (self.bg0_x + self.bg0.get_width(), 0))
-        surface.blit(self.bg1, (self.bg1_x, 0))
-        surface.blit(self.bg1, (self.bg1_x + self.bg1.get_width(), 0))
-        surface.blit(self.bg2, (self.bg2_x, 0))
-        surface.blit(self.bg2, (self.bg2_x + self.bg2.get_width(), 0))
-        # Temporary text
-        Text.draw_text(surface, "LOFI SAMURAI", (0, 0, 0), 
-                      surface.get_width() // 2, 30)
+    def render(self, surface: pygame.Surface) -> None:
+        self.draw_bg(surface)
+        Text.draw_title(surface, "LOFI SAMURAI", surface.get_width() // 2, surface.get_height() // 16)
+        self.draw_ui(surface)
         
-        # Draw UI
-        surface.blit(self.ui_1, (surface.get_width()//2 - self.ui_1.get_width()//2, surface.get_height()//8))
-        
-    def scroll_bg(self, delta_time):
+    def scroll_bg(self, delta_time: float):
         # Scroll speeds
         bg0_speed, bg1_speed, bg2_speed = 10,20,30
-        
         # Scroll bg to left
         self.bg0_x -= bg0_speed * delta_time
         self.bg1_x -= bg1_speed * delta_time
         self.bg2_x -= bg2_speed * delta_time
-        
         # Wrap
         bg_width = self.bg0.get_width()
         if self.bg0_x <= -bg_width:
@@ -61,3 +57,30 @@ class TitleState(State):
             self.bg1_x = 0
         if self.bg2_x <= -bg_width:
             self.bg2_x = 0
+    
+    def draw_bg(self, surface: pygame.Surface):
+        # Drawing each image a second time offset to remove scoll seams
+        surface.blit(self.bg0, (self.bg0_x, 0))
+        surface.blit(self.bg0, (self.bg0_x + self.bg0.get_width(), 0))
+        surface.blit(self.bg1, (self.bg1_x, 0))
+        surface.blit(self.bg1, (self.bg1_x + self.bg1.get_width(), 0))
+        surface.blit(self.bg2, (self.bg2_x, 0))
+        surface.blit(self.bg2, (self.bg2_x + self.bg2.get_width(), 0))
+        
+    def draw_ui(self, surface: pygame.Surface):
+        # Big box
+        big_box_x = (surface.get_width() - self.ui_big_box.get_width()) // 2
+        big_box_y = int(surface.get_height() * 0.15)
+        surface.blit(self.ui_big_box, (big_box_x, big_box_y))
+        # Buttons
+        button_x = big_box_x + (self.ui_big_box.get_width() - self.ui_button.get_width()) // 2
+        total_button_height = self.ui_button.get_height() * 4
+        spacing = (self.ui_big_box.get_height() - total_button_height) // 5
+        button_y = big_box_y + spacing
+        surface.blit(self.ui_play_button, (button_x, button_y))
+        button_y += self.ui_button.get_height() + spacing
+        surface.blit(self.ui_load_button, (button_x, button_y))
+        button_y += self.ui_button.get_height() + spacing
+        surface.blit(self.ui_options_button, (button_x, button_y))
+        button_y += self.ui_button.get_height() + spacing
+        surface.blit(self.ui_quit_button, (button_x, button_y))
