10171a7e3e7ae5d37cf600b3319a55a1bbf57f96
Foster <fosterjameskiatarae@gmail.com>
2025-10-23 22:09:07 +1300

    feat(game.py): Implement basic title screen state. Add current state render and update methods into main loop.
    Was calling pygame.quit only when the x button on the window was being called, moved the quit call to outside the main game loop. If anything needs to quit  just make it set game.playing to false, loop will just cancel itself.

--- scripts/game.py
+++ scripts/game.py
@@ -2,15 +2,16 @@
 # Game class that brings all modules together and runs the main gameplay loop.
 # Author: Foster Rae
 # Created: 18-10-2025
-# Last Modified: 18-10-2025
+# Last Modified: 23-10-2025
 import os, time, pygame
 from scripts.config import Config
 from scripts.components.player_controller import PlayerController
 from scripts.utilities.asset_manager import AssetManager
-
+from scripts.screen_states.title_state import TitleState
 class Game():
     def __init__(self):
         pygame.init()
+        AssetManager.load_core_assets()
         # Screen
         self.GAME_W, self.GAME_H = Config.GAME_W, Config.GAME_H
         self.SCREEN_W, self.SCREEN_H = Config.SCREEN_W, Config.SCREEN_H
@@ -20,11 +21,13 @@ class Game():
         self.screen_state_stack = []
         # Timing
         self.clock = pygame.time.Clock()
+        self.dt = 0.0
         # Others
         self.running, self.playing = True, True
         self.controller = PlayerController()
         
-        AssetManager.load_core_assets()
+        self.load_states()
+        
         # TEMPORARY, REMOVE
         AssetManager.load_level_assets('stringstar_fields')
         
@@ -32,13 +35,14 @@ class Game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running, self.playing = False, False
-                pygame.quit()
             self.controller.handle_input(event)
     
     def update(self):
-        self.clock.tick(self.fps)
+        self.dt = self.clock.tick(self.fps)
+        self.screen_state_stack[-1].update(self.dt, self.controller.actions)
     
     def render(self):
+        self.screen_state_stack[-1].render(self.game_canvas)
         self.screen.blit(pygame.transform.scale(self.game_canvas,(self.SCREEN_W,self.SCREEN_H)), (0,0))
         pygame.display.flip()
         
@@ -47,6 +51,12 @@ class Game():
             self.handle_events()
             self.update()
             self.render()
+        pygame.quit()
+            
+    def load_states(self):
+        # Load title screen first
+        self.title_screen = TitleState(self)
+        self.screen_state_stack.append(self.title_screen)
                 
             
                 
