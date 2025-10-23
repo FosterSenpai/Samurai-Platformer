995cf5f86d057ff8384d90de690c509e4df04e05
Foster <fosterjameskiatarae@gmail.com>
2025-10-24 01:51:43 +1300

    feat(text.py): Add methods for drawing with different font sizes.

--- scripts/utilities/text.py
+++ scripts/utilities/text.py
@@ -11,7 +11,6 @@ class Text:
     @staticmethod
     def draw_text(surface:pygame.Surface, text: str, colour, x, y):
         """Draw text at a location on screen.  
-        Using default font right now.
         Args:
             surface (pygame.Surface): The surface to draw the text on.
             text (str): The text to draw.
@@ -20,6 +19,34 @@ class Text:
             y (_type_): The y coordinate of the center of the text.
         """
         text_surface = AssetManager.core_fonts['default'].render(text, True, colour)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x,y)
+        surface.blit(text_surface, text_rect)
+        
+    @staticmethod
+    def draw_title(surface: pygame.Surface, text: str, x: int, y: int):
+        """Draw text at a location on screen with large title font.
+        Args:
+            surface (pygame.Surface): The surface to draw the text on.
+            text (str): The text to draw.
+            x (int): The x coordinate of the center of the text.
+            y (int): The y coordinate of the center of the text.
+        """
+        text_surface = AssetManager.core_fonts['title'].render(text, True, (175,175,175))
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x,y)
+        surface.blit(text_surface, text_rect)
+    
+    @staticmethod
+    def draw_small(surface: pygame.Surface, text: str, x: int, y: int):
+        """Draw text at a location on screen with small font.
+        Args:
+            surface (pygame.Surface): The surface to draw the text on.
+            text (str): The text to draw.
+            x (int): The x coordinate of the center of the text.
+            y (int): The y coordinate of the center of the text.
+        """
+        text_surface = AssetManager.core_fonts['subtitle'].render(text, True, (200,200,200))
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
         surface.blit(text_surface, text_rect)
