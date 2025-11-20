cc8d89faaf8b1c24127082de3e5b8751075c3b55
Foster <fosterjameskiatarae@gmail.com>
2025-11-20 14:56:34 +1300

    feat(asset_manager.py) split music and sounds loading, added music handling to asset player with play, fade and stop functions

--- scripts/utilities/asset_manager.py
+++ scripts/utilities/asset_manager.py
@@ -2,12 +2,16 @@ import pygame
 import os
 from scripts.config import Config
 from scripts.utilities.spritesheet import Spritesheet
+from typing import Optional
 
 class AssetManager:
     # Core assets to always be loaded
-    player_animations = {}
+    player_animations: dict[str, list[pygame.Surface]] = {}
+    # TODO: lock the rest of these  vars
     core_sprites = {}
     core_sounds = {}
+    core_music: dict[str, str] = {} # track name - file path
+    current_track: Optional[str] = None
     core_fonts = {}
     
     # Level specific assets to be loaded and unloaded per level
@@ -20,6 +24,7 @@ class AssetManager:
     assets_dir = os.path.join(data_dir, 'assets')
     fonts_dir = os.path.join(assets_dir, 'fonts')
     sounds_dir = os.path.join(assets_dir, 'sounds')
+    music_dir = os.path.join(sounds_dir, 'music')
     sprites_dir = os.path.join(assets_dir, 'sprites')
     ui_sprites_dir = os.path.join(sprites_dir, 'ui')
     environment_sprites_dir = os.path.join(sprites_dir, 'environment')
@@ -52,36 +57,15 @@ class AssetManager:
         # Sounds
         AssetManager.core_sounds['button_click'] = pygame.mixer.Sound(os.path.join(AssetManager.sounds_dir, 'ui', 'button_click.ogg'))
         AssetManager.core_sounds['notification'] = pygame.mixer.Sound(os.path.join(AssetManager.sounds_dir, 'ui', 'notification.ogg'))
-        # Storing paths for music, music is loaded per screen/state
-        AssetManager.core_sounds['evening mood'] = os.path.join(AssetManager.sounds_dir, 'music/evening mood.ogg')
+        # Music
+        AssetManager.load_all_music_paths()
         
         # Load player animations
         AssetManager.load_player_sprites()
-    
-    @staticmethod
-    def load_level_assets(level_name: str) -> None:
-        """Load level specific assets.
-        Args:
-            level_name (str): The name of the level to load the assets for.
-        """
-        match(level_name):
-            case 'starstring_fields':
-                AssetManager.level_sprites['bg0'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_sprites_dir, 'background', '0.png'))
-                AssetManager.level_sprites['bg1'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_sprites_dir, 'background', '1.png'))
-                AssetManager.level_sprites['bg2'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_sprites_dir, 'background', '2.png'))
-                
-    
-    @staticmethod
-    def unload_level_assets() -> None:
-        """Unload any level specific assets."""
-        AssetManager.level_fonts.clear()
-        AssetManager.level_sounds.clear()
-        AssetManager.level_sprites.clear()
         
     @staticmethod
     def load_player_sprites() -> None:
         """Load all player sprites."""
-        player_animations: dict[str, list[pygame.Surface]] = {} # Animation name - frames list
         anim_dir = os.path.join(AssetManager.sprites_dir, 'entities', 'player')
         idle_sheet = Spritesheet(os.path.join(anim_dir, 'IDLE.png'), frame_count=10, frame_size=(96,96))
         walk_sheet = Spritesheet(os.path.join(anim_dir, 'WALK.png'), frame_count=12, frame_size=(96,96))
@@ -95,4 +79,70 @@ class AssetManager:
         AssetManager.player_animations['jump'] = jump_sheet.parse_frames()
         AssetManager.player_animations['dash'] = dash_sheet.parse_frames()
         
+    @staticmethod
+    def load_all_music_paths() -> None:
+        """Load all music paths into core_sounds dict."""
+        for file in os.listdir(AssetManager.music_dir):
+            if file.endswith('.ogg'):
+                track_name = file.replace('.ogg','')
+                AssetManager.core_music[track_name] = os.path.join(AssetManager.music_dir, file)
         
+    @staticmethod
+    def load_level_assets(level_name: str) -> None:
+        """Load level specific assets.
+        Args:
+            level_name (str): The name of the level to load the assets for.
+        """
+        match(level_name):
+            case 'starstring_fields':
+                AssetManager.level_sprites['bg0'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_sprites_dir, 'background', '0.png'))
+                AssetManager.level_sprites['bg1'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_sprites_dir, 'background', '1.png'))
+                AssetManager.level_sprites['bg2'] = pygame.image.load(os.path.join(AssetManager.stringstar_fields_sprites_dir, 'background', '2.png'))
+        
+    @staticmethod
+    def unload_level_assets() -> None:
+        """Unload any level specific assets."""
+        AssetManager.level_fonts.clear()
+        AssetManager.level_sounds.clear()
+        AssetManager.level_sprites.clear()
+        
+    @staticmethod
+    def unload_core_assets() -> None:
+        """Unload core assets."""
+        AssetManager.core_fonts.clear()
+        AssetManager.core_sounds.clear()
+        AssetManager.core_sprites.clear()
+        AssetManager.core_music.clear()
+        AssetManager.player_animations.clear()
+        
+    # MUSIC CONTROLS
+    @staticmethod
+    def play_music(track_name: str, volume: float = 0.5, loops: int = -1) -> None:
+        """Play music track by name.
+        Args:
+            track_name (str): The name of the track to play.
+            loops (int): Number of loops (-1 for infinite).
+            volume (float): Volume level (0.0 to 1.0).
+        """
+        if track_name in AssetManager.core_music:
+            AssetManager.current_track = track_name
+            pygame.mixer.music.load(AssetManager.core_music[track_name])
+            pygame.mixer.music.set_volume(volume)
+            pygame.mixer.music.play(loops)
+        else:
+            print(f"Music track '{track_name}' not found!")
+            
+    @staticmethod
+    def fade_music(duration_ms: int) -> None:
+        """Fade out current music over duration.
+        Args:
+            duration_ms (int): Duration in milliseconds to fade out.
+        """
+        pygame.mixer.music.fadeout(duration_ms)
+        AssetManager.current_track = None
+        
+    @staticmethod
+    def stop_music() -> None:
+        """Stop current music immediately."""
+        pygame.mixer.music.stop()
+        AssetManager.current_track = None
